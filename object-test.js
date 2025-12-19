db06886721cd2895496e0efefb5ddc89a3289366
elephantfive <dylan.chilcoat@gmail.com>
2025-12-19 11:05:27 -0600

    Add form for submitting new books

--- object-test.js
+++ object-test.js
@@ -43,7 +43,21 @@ function displayLibrary() {
 }
 
 function addNewBook() {
+    const newBookDialog = document.querySelector("dialog")
+    const dialogText = document.querySelector(".dialog");
     const newBookButton = document.querySelector(".add-new-book");
+    const nextButton = document.querySelector(".next");
+    const newBookInput = document.querySelector("input");
+
+    newBookButton.addEventListener("click", function(){
+        newBookDialog.showModal();
+    });
+    
+    nextButton.addEventListener("click", function(){
+
+    });
+
+    const newBookInfo = [];
 }
 
 const myLibrary =[];
@@ -52,3 +66,4 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 512, false);
 addBookToLibrary("Bitlop", "J. F. bOOBAH", 123, true);
 
 displayLibrary();
+addNewBook();
