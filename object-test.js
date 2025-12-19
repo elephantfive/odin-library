59bd47ed22c9457db194dfd8f768d80757dc671a
elephantfive <dylan.chilcoat@gmail.com>
2025-12-19 10:24:44 -0600

    Add very basic grid CSS and JS for book display

--- object-test.js
+++ object-test.js
@@ -1,4 +1,4 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     if (!new.target) {
         throw Error("You must use the 'new' operator.");
     }
@@ -6,15 +6,49 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.id = crypto.randomUUID;
-    this.info = function () {
-        return this.read ? `${this.title} by ${this.author}, ${this.pages}, read.`
-        :`${this.title} by ${this.author}, ${this.pages}, not read yet.`;
-    };
+    this.id = id;
+    this.info = [author, `${pages} pages`];
+    if (read) {
+        this.info.push("Read.");
+    } else {
+        this.info.push("Not read yet.");
+    }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read, crypto.randomUUID));
+}
+
+function displayLibrary() {
+    const books = document.querySelector(".books");
+    for (const book of myLibrary) {
+        const newBook = document.createElement("div");
+        newBook.classList.add("book");
+
+        const newBookTitle = document.createElement("h2");
+        newBookTitle.textContent = book.title;
+
+        const newBookInfo = document.createElement('ul');
+        for (const item of book.info) {
+            const newListItem = document.createElement("li");
+            newListItem.textContent = item;
+            newBookInfo.appendChild(newListItem);
+        }
+
+        books.appendChild(newBook);
+        newBook.appendChild(newBookTitle);
+        newBook.appendChild(newBookInfo);
+
+    }
+}
+
+function addNewBook() {
+    const newBookButton = document.querySelector(".add-new-book");
 }
 
 const myLibrary =[];
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 512, false);
+addBookToLibrary("Bitlop", "J. F. bOOBAH", 123, true);
+
+displayLibrary();
